11a18b10c61e1daf57e43317e828c4982701b533
tjh <1937471723@qq.com>
2019-05-31 14:36:55 +0800

    led灯

--- main/main.js
+++ main/main.js
@@ -1,4 +1,30 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(a) {
+    let arr = [ ["._.","|.|", "|_|" ], ["...","..|", "..|"] ,["._.","._|","|_."],
+        ["._.","._|","._|"],["...","|_|","..|"],["._.","|_.","._|"],
+        ["._.","|_.","|_|"],["._.","..|","..|"],["._.","|_|","|_|"],["._.","|_|","..|"]];
+    let len = a.length;
+    let str="";
+    for(let i=0;i<len;i++){
+        if(i==len-1){
+            str += arr[a[i]][0]+"\n";
+        }else{
+            str += arr[a[i]][0]+" ";
+        }
+    }
+    for(let i=0;i<len;i++){
+        if(i==len-1){
+            str += arr[a[i]][1]+"\n";
+        }else{
+            str += arr[a[i]][1]+" ";
+        }
+    }
+    for(let i=0;i<len;i++){
+        if(i==len-1){
+            str += arr[a[i]][2]+"\n";
+        }else{
+            str += arr[a[i]][2]+" ";
+        }
+    }
+    console.log(str);
+    return str;
 };
